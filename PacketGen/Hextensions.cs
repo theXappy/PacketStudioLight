c65494cfbbfe72bbc1f2edc65dca664ca4c20a02
Xappy <noforu@gmail.com>
2023-01-28 16:26:51 +0200

    Hex Parser: Ignore whitespaces

--- PacketGen/Hextensions.cs
+++ PacketGen/Hextensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,8 @@ namespace PacketGen
 
         public static byte[] GetBytesFromHex(string hex)
         {
+            hex = hex.Replace(" ", string.Empty);
+            hex = hex.Replace("\t", string.Empty);
             return Enumerable.Range(0, hex.Length)
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
